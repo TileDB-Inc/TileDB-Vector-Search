fadbe41eaa2602ee4413c3083e072c4edb175e4f
Andrew Lumsdaine <al75@uw.edu>
2023-05-28 13:35:02 -0700

    Experimental performance results [skip ci]

---
+++ experimental/include/test/gen_queries.cc
@@ -0,0 +1,49 @@
+
+#include <catch2/catch_all.hpp>
+#include <thread>
+#include "utils/timer.h"
+
+#include "flat_query.h"
+#include "ivf_query.h"
+#include "linalg.h"
+
+bool global_debug = false;
+std::string global_region = "us-east-1";
+
+TEST_CASE("gen db", "[queries]") {
+  size_t dimension = 128;
+
+  size_t db = GENERATE(1, 10, 100, 1000, 10000, 100000, 1000000, 10000000);
+
+  std::cout << "db: " << db << std::endl;
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<int> dist(-127, 127);
+
+  auto db_mat = ColMajorMatrix<float>(dimension, db);
+  for (auto& x : raveled(db_mat)) {
+    x = dist(gen);
+  }
+  std::string db_name = "db_" + std::to_string(db) + ".tdb";
+  write_matrix(db_mat, db_name);
+}
+
+TEST_CASE("gen q", "[queries]") {
+  size_t dimension = 128;
+
+  size_t q = GENERATE(1, 10, 100, 1000, 10000);
+
+  std::cout << "q: " << q << std::endl;
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<int> dist(-127, 127);
+
+  auto q_mat = ColMajorMatrix<float>(dimension, q);
+  for (auto& x : raveled(q_mat)) {
+    x = dist(gen);
+  }
+  std::string q_name = "q_" + std::to_string(q) + ".tdb";
+  write_matrix(q_mat, q_name);
+}
--- experimental/include/test/time_queries.cc
+++ experimental/include/test/time_queries.cc
@@ -2,11 +2,17 @@
 
 #include <catch2/catch_all.hpp>
 #include <thread>
+
+#include <iostream>
+#include <filesystem>
 #include "utils/timer.h"
 
 #include "flat_query.h"
 #include "ivf_query.h"
 #include "linalg.h"
+
+bool global_debug = false;
+std::string global_region = "us-east-1";
 
 // Cases:
 //    Big db / small db
@@ -14,9 +20,11 @@
 //    big k / small k
 //    nth = true / false
 
+#if 0
 TEST_CASE("time queries", "[queries]") {
   size_t dimension = 128;
   size_t nthreads = std::thread::hardware_concurrency();
+
 #if 1
   size_t small_threads = 1;
   size_t med_threads = nthreads / 4;
@@ -121,5 +129,85 @@ TEST_CASE("time queries", "[queries]") {
         }
       }
     }
+  }
+}
+
+#endif
+
+TEST_CASE("time queries", "[queries]") {
+  size_t dimension = 128;
+
+  // db, q, k, nth, nthreads
+
+
+  size_t nthreads = GENERATE(1, 8);
+
+  size_t k = GENERATE(1, 10, 100);
+
+  std::tuple<size_t, size_t> expts = GENERATE(
+
+      std::make_tuple(10000000, 1),
+      std::make_tuple(1, 10000),
+
+      std::make_tuple(1000000, 10),
+      std::make_tuple(10, 10000),
+
+      std::make_tuple(1000000, 100),
+      std::make_tuple(100, 1000),
+
+      std::make_tuple(100000, 1000),
+      std::make_tuple(1000, 1000)
+  );
+  size_t nth = GENERATE(true, false);
+
+
+
+  std::cout << "\n=======================================================\n";
+
+  auto&& [db, q] = expts;
+  if (k <= db) {
+    //  std::filesystem::path currentPath = std::filesystem::current_path();
+    // std::cout << "Current working directory: " << currentPath << std::endl;
+
+    std::string db_name = "db_" + std::to_string(db) + ".tdb";
+    std::string q_name = "q_" + std::to_string(q) + ".tdb";
+
+    // std::cout << "db_name: " << db_name << " q_name: " << q_name << "\n";
+
+    auto db_mat = tdbColMajorMatrix<float>(db_name);
+
+    auto q_mat = tdbColMajorMatrix<float>(q_name);
+
+    std::cout << "\n# [ Experiment: ]: nthreads: " << nthreads << " q: " << q
+              << " db: " << db << " k: " << k << " nth: " << nth << "\n\n";
+
+    if constexpr (false)
+    {
+      life_timer _outer{"qv_query"};
+      qv_query(db_mat, q_mat, k, nthreads);
+    }
+    if constexpr (false)
+    {
+      life_timer _outer{"qv_query_nth"};
+      qv_query_nth(db_mat, q_mat, k, nth, nthreads);
+    }
+    {
+      life_timer _outer{"vq_query_heap"};
+      vq_query_heap(db_mat, q_mat, k, nthreads);
+    }
+    {
+      life_timer _outer{"vq_query_nth"};
+      vq_query_nth(db_mat, q_mat, k, nth, nthreads);
+    }
+    {
+      life_timer _outer{"gemm_query"};
+      gemm_query(db_mat, q_mat, k, nth, nthreads);
+    }
+#if 0
+  {
+    life_timer _outer{"blocked_gemm_query"};
+    blocked_gemm_query(b_db_mat, q_mat, k, nth, nthreads);
+  }
+#endif
   }
 }
