cd966e2aa67c8f007040a7a00d9221edbe8d462a
Andrew Lumsdaine <al75@uw.edu>
2023-06-01 23:18:59 -0700

    fix small recall bug [skip ci]

--- experimental/src/ivf_hack.cc
+++ experimental/src/ivf_hack.cc
@@ -146,45 +146,30 @@ int main(int argc, char* argv[]) {
   auto centroids = tdbColMajorMatrix<centroids_type>(centroids_uri);
   debug_matrix(centroids, "centroids");
 
-#if 0
-  if (is_local_array(centroids_uri) && !std::filesystem::exists(db_uri)) {
-    std::cerr << "Error: db URI does not exist: " << args["--centroids_uri"]
-              << std::endl;
-    return 1;
-  }
-#endif
-
   float recall{0.0f};
   json recalls;
 
-  // Function for finding the top k nearest neighbors accelerated by kmeans
-  // @todo Move this to a self-contained function
+  // Find the top k nearest neighbors accelerated by kmeans and do some reporting
   {
     life_timer _("query_time");
 
+    // @todo Encapsulate these arrays in a class
     // auto shuffled_db = tdbColMajorMatrix<shuffled_db_type>(part_uri);
-    // auto indices = tdbMatrix<size_t, Kokkos::layout_left>(index_uri);
-    auto indices = read_vector<indices_type>(index_uri);
     // auto shuffled_ids = read_vector<shuffled_ids_type>(id_uri);
-
     // debug_matrix(shuffled_db, "shuffled_db");
-    debug_matrix(indices, "indices");
     // debug_matrix(shuffled_ids, "shuffled_ids");
+
+    auto indices = read_vector<indices_type>(index_uri);
+    debug_matrix(indices, "indices");
 
     auto q = tdbColMajorMatrix<q_type>(query_uri, nqueries);
     debug_matrix(q, "q");
 
-    // What should be returned here?  Maybe a pair with the ids and scores?
     auto top_k = kmeans_query(
         part_uri, centroids, q, indices, id_uri, nprobe, k_nn, nth, nthreads);
 
     debug_matrix(top_k, "top_k");
 
-
-        // Once this is a function, simply return kmeans_ids
-    // For now, print the results to std::cout
-    // @todo also get scores
-    // @todo add an output_uri argument
     if (args["--groundtruth_uri"]) {
       auto groundtruth_uri = args["--groundtruth_uri"].asString();
 
@@ -208,7 +193,7 @@ int main(int argc, char* argv[]) {
       size_t total_groundtruth = top_k.num_cols() * top_k.num_rows();
       for (size_t i = 0; i < top_k.num_cols(); ++i) {
         std::sort(begin(top_k[i]), end(top_k[i]));
-        std::sort(begin(groundtruth[i]), end(groundtruth[i]));
+        std::sort(begin(groundtruth[i]), begin(groundtruth[i]) + k_nn);
         debug_matrix(top_k, "top_k");
         debug_slice(top_k, "top_k");
         total_intersected += std::set_intersection(
@@ -231,12 +216,8 @@ int main(int argc, char* argv[]) {
   auto timings = get_timings();
 
   if (global_verbose) {
-    // Report these results in a table
-    // algorithm dataset nqueries nprobe top-n ms qps recall
-    // # [ In memory portion of kmeans_query_small_q ]
-    // std::cout << std::setw() << std::left  usw
-    //
 
+    // Quick and dirty way to get query info in summarizable form
     auto ms = timings["# [ In memory portion of kmeans_query_small_q ]"];
     auto qps = ((float)nqueries) / ((float)ms / 1000.0);
     std::cout << std::setw(8) << nqueries;
