2c522d1815f2d70b24ebd1768800264a114d2f19
Isaiah Norton <isaiah.norton@gmail.com>
2023-06-05 15:10:29 -0400

    Misc

--- apis/python/test/test_index.py
+++ apis/python/test/test_index.py
@@ -9,13 +9,13 @@ def test_flat_index(tmpdir):
     #ground_truth_uri = "s3://tiledb-andrew/sift/sift_groundtruth"
 
     db_uri = "~/work/proj/vector-search/datasets/sift-andrew/sift_base"
-    ground_truth_uri = "/Users/inorton/work/proj/vector-search/datasets/sift-andrew/sift_groundtruth"
+    ground_truth_uri = "~/work/proj/vector-search/datasets/sift-andrew/sift_groundtruth"
     query_uri = "~/work/proj/vector-search/datasets/sift-andrew/sift_query"
 
     query_vectors = vs.load_as_array(query_uri)[:,:10]
 
     index = FlatIndex(db_uri)
     result = index.query(query_vectors)
+    assert isinstance(result, np.ndarray)
 
     #ground_truth = vs.load_as_array(ground_truth_uri)
-    assert isinstance(result, np.ndarray)
