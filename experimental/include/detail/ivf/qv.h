8545935205c62284a82dc36e5f14fbcb2c89f793
Andrew Lumsdaine <al75@uw.edu>
2023-06-06 05:46:25 +0000

    Some updates on EC2 for benchmarking [skip ci]

--- experimental/include/detail/ivf/qv.h
+++ experimental/include/detail/ivf/qv.h
@@ -77,9 +77,9 @@ auto qv_query_heap(
   auto top_centroids =
       detail::flat::qv_query_nth(centroids, q, nprobe, false, nthreads);
 
-  //  auto min_scores = std::vector<fixed_min_pair_heap<float, size_t>>(
-  //      size(q), fixed_min_pair_heap<float, size_t>(k_nn));
-  auto min_scores = std::vector<fixed_min_heap<std::pair<float, size_t>>>(size(q), fixed_min_heap<std::pair<float, size_t>>(k_nn));
+  auto min_scores = std::vector<fixed_min_pair_heap<float, size_t>>(
+       size(q), fixed_min_pair_heap<float, size_t>(k_nn));
+  // auto min_scores = std::vector<fixed_min_heap<std::pair<float, size_t>>>(size(q), fixed_min_heap<std::pair<float, size_t>>(k_nn));
 
   life_timer __{std::string{"In memory portion of "} + tdb_func__};
   auto par = stdx::execution::indexed_parallel_policy{nthreads};
@@ -90,9 +90,9 @@ auto qv_query_heap(
           size_t stop = indices[top_centroids(p, j) + 1];
 
           for (size_t i = start; i < stop; ++i) {
-            auto score = L2(q[j], shuffled_db[i]);
-            // min_scores[j].insert(score, shuffled_ids[i]);
-	    min_scores[j].insert({score, shuffled_ids[i]});
+            auto score = L2(q_vec/*q[j]*/, shuffled_db[i]);
+	    min_scores[j].insert(score, shuffled_ids[i]);
+	    // min_scores[j].insert({score, shuffled_ids[i]});
           }
         }
       });
