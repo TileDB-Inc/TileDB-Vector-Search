200b0a81d76ab06ffa457446c70dc3ee428645ba
Isaiah Norton <isaiah.norton@gmail.com>
2023-06-08 01:41:34 -0400

    WIP pip support

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -1,10 +1,14 @@
 from skbuild import setup  # This line replaces 'from setuptools import setup'
 
+import os
+
 setup(
     name="tiledb-vector-search",
     description="Vector Search with TileDB",
     author='TileDB',
     license="MIT",
+    python_requires="~=3.7",
     packages=['tiledb.vector_search'],
-    python_requires=">=3.7",
+    cmake_source_dir="../../experimental",
+    cmake_args=['-DTILEDB_VS_PYTHON=ON'],
 )
