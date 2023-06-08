83ff4fcf18c50d33a429b93975cbdcfd22882fe6
Isaiah Norton <isaiah.norton@gmail.com>
2023-06-08 00:17:14 -0400

    Fix skbuild invocation from pip

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -2,7 +2,6 @@ from skbuild import setup  # This line replaces 'from setuptools import setup'
 
 setup(
     name="tiledb-vector-search",
-    version="0.1",
     description="Vector Search with TileDB",
     author='TileDB',
     license="MIT",
