79fc35efaf3ff5edbb988cbe0ab4d2a99e8e6db8
Isaiah Norton <isaiah.norton@gmail.com>
2023-06-09 10:17:59 -0400

    Add support for overriding TileDB_DIR from env

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -2,6 +2,11 @@ from skbuild import setup  # This line replaces 'from setuptools import setup'
 
 import os
 
+cmake_args = ['-DTILEDB_VS_PYTHON=ON']
+if tiledb_dir := os.environ.get("TILEDB_DIR", None):
+    cmake_args.append(f"-DTileDB_DIR={tiledb_dir}")
+
+
 setup(
     name="tiledb-vector-search",
     description="Vector Search with TileDB",
@@ -10,7 +15,7 @@ setup(
     packages=['tiledb.vector_search'],
     package_dir={"": "src"},
     cmake_source_dir="../../experimental",
-    cmake_args=['-DTILEDB_VS_PYTHON=ON'],
+    cmake_args=cmake_args,
     cmake_install_target="install-libtiledbvectorsearch",
     cmake_install_dir="src/tiledb/vector_search"
 )
