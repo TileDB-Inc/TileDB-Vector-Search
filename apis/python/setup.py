e6c620ca534f5a5d978efaf0bf9e54c8dce28193
Isaiah Norton <isaiah.norton@gmail.com>
2023-06-08 22:23:51 -0400

    Fix up editable install

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -6,9 +6,11 @@ setup(
     name="tiledb-vector-search",
     description="Vector Search with TileDB",
     author='TileDB',
-    license="MIT",
     python_requires="~=3.7",
     packages=['tiledb.vector_search'],
+    package_dir={"": "src"},
     cmake_source_dir="../../experimental",
     cmake_args=['-DTILEDB_VS_PYTHON=ON'],
+    cmake_install_target="install-libtiledbvectorsearch",
+    cmake_install_dir="src/tiledb/vector_search"
 )
