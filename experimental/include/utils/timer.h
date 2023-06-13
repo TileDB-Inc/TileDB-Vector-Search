9ad1168aae321aac034d020517e185df1e1ee9c9
Isaiah Norton <ihnorton@users.noreply.github.com>
2023-06-13 11:04:39 -0400

    Update experimental/include/utils/timer.h
    Co-authored-by: Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>

--- experimental/include/utils/timer.h
+++ experimental/include/utils/timer.h
@@ -154,7 +154,7 @@ std::ostream& operator<<(std::ostream& os, const us_timer& t) {
 #ifndef tdb_func__
 #ifdef __cpp_lib_source_location
 #include <source_location>
-#define tdb_func__ (std::source_location::current().function_name())
+#define tdb_func__ std::string{std::source_location::current().function_name()}
 #else
 #define tdb_func__ \
   std::string {    \
