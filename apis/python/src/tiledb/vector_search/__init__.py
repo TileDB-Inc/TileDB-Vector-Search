f3a3feb633aeaf24fabe82763f70cd24775584c4
Isaiah Norton <ihnorton@users.noreply.github.com>
2023-06-14 10:35:07 -0400

    Reformat all files with Black (#33)

--- apis/python/src/tiledb/vector_search/__init__.py
+++ apis/python/src/tiledb/vector_search/__init__.py
@@ -2,10 +2,4 @@ from .ingestion import ingest
 from .module import load_as_array, load_as_matrix
 from .index import FlatIndex
 
-__all__ = [
-    "FlatIndex",
-    "load_as_array",
-    "load_as_matrix",
-    "ingest",
-    "query_vq"
-]
+__all__ = ["FlatIndex", "load_as_array", "load_as_matrix", "ingest", "query_vq"]
