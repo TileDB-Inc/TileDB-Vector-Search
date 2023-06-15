f6924c7648ee72f0deed19b99321e64a0695896f
Andrew Lumsdaine <al75@uw.edu>
2023-06-15 14:17:11 -0700

    Add document header [skip ci]

--- experimental/include/array_types.h
+++ experimental/include/array_types.h
@@ -1,3 +1,36 @@
+/**
+ * @file   array_types.h
+ *
+ * @section LICENSE
+ *
+ * The MIT License
+ *
+ * @copyright Copyright (c) 2023 TileDB, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @section DESCRIPTION
+ *
+ * Some hard-coded data types based on our current schema for query search.
+ * Will be expanded and generalized.
+ *
+ */
 
 #ifndef TDB_ARRAY_TYPES_H
 #define TDB_ARRAY_TYPES_H
@@ -5,36 +38,25 @@
 #include <cstdint>
 
 /**
- * In general for our similarity computations, large sets of vectors are stored
- * using 8 bits per vector element.  Occasionally, smaller datsets will be
- * stored as float.  Ideally, we would support either type (likely using some
- * form of type erasure).  But for now, we just use a sledgehammer.
- *
  * @todo Use size_t instead of uint64_t?
  */
 #if 1
-
 using db_type = uint8_t;
-using shuffled_db_type = uint8_t;
-using shuffled_ids_type = uint64_t;
-using indices_type = uint64_t;
-using parts_type = uint64_t;
-using centroids_type = float;
-using q_type = uint8_t;
-using groundtruth_type = int32_t;
-using original_ids_type = uint64_t;
-
 #else
-
 using db_type = float;
-using shuffled_db_type = float;
-using shuffled_ids_type = uint64_t;
-using indices_type = uint64_t;
-using parts_type = uint64_t;
-using centroids_type = float;
-using q_type = float;
+#endif
+
+using q_type = db_type;
 using groundtruth_type = int32_t;
 
-#endif
+using centroids_type = float;
+using shuffled_db_type = db_type;
+
+// @todo Should be the same as groundtruth_type
+using shuffled_ids_type = uint64_t;
+
+// @todo Are these the same?
+using indices_type = uint64_t;
+using parts_type = uint64_t;
 
 #endif  // TDB_ARRAY_TYPES_H
