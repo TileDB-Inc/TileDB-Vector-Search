7019e1b3e72fa61bc47732c598d8daff4bcb8ac8
Isaiah Norton <isaiah@tiledb.com>
2023-06-17 21:23:03 +0000

    Bump Catch2 version to 3.3.2 to fix GCC 13 build

--- src/cmake/Catch2.cmake
+++ src/cmake/Catch2.cmake
@@ -8,7 +8,7 @@ include(FetchContent)
 FetchContent_Declare(
   catch2
   GIT_REPOSITORY "https://github.com/catchorg/Catch2.git"
-  GIT_TAG        "v3.1.0"
+  GIT_TAG        "v3.3.2"
 )
 
 FetchContent_MakeAvailable(catch2)
