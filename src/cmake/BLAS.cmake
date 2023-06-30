7cdba14670dd669b21b3b3fe59a9d86b45199a96
Isaiah Norton <isaiah@tiledb.com>
2023-06-30 19:50:07 +0000

    Fix finding OpenBLAS cblas.h

--- src/cmake/BLAS.cmake
+++ src/cmake/BLAS.cmake
@@ -23,13 +23,28 @@ else()
     set (BLA_VENDOR OpenBLAS)
   endif()
 
-  include(FindBLAS)
+  find_package(BLAS REQUIRED)
 
   if (BLAS_FOUND)
-    message(STATUS "BLAS FOUND ${BLAS_FOUND}")
-    message(STATUS "${BLAS_LINKER_FLAGS}")
-    message(STATUS "${BLAS_LIBRARIES}")
-    target_link_libraries(flat_blas INTERFACE $<LINK_ONLY:BLAS::BLAS>)
+    message(STATUS "BLAS_FOUND ${BLAS_FOUND}, BLAS_VENDOR: ${BLA_VENDOR}")
+    message(STATUS "  BLAS_LINKER_FLAGS: ${BLAS_LINKER_FLAGS}")
+    message(STATUS "  BLAS_LIBRARIES: ${BLAS_LIBRARIES}")
+
+    if (NOT DEFINED BLAS_INCLUDE_DIR)
+        # https://gitlab.kitware.com/cmake/cmake/-/issues/20268
+        cmake_path(GET BLAS_LIBRARIES PARENT_PATH PARENT_DIR)
+        cmake_path(GET PARENT_DIR PARENT_PATH BLAS_SEARCH_ROOT)
+        find_path(BLAS_INCLUDE_DIR
+          NAMES cblas.h
+          PATHS ${BLAS_SEARCH_ROOT}
+          PATH_SUFFIXES include/openblas include
+          NO_DEFAULT_PATH
+        )
+    endif()
+    message(STATUS "  BLAS_INCLUDE_DIR: ${BLAS_INCLUDE_DIR}")
+
+    target_include_directories(flat_blas INTERFACE ${BLAS_INCLUDE_DIR})
+    target_link_libraries(flat_blas INTERFACE BLAS::BLAS)
     target_compile_definitions(flat_blas INTERFACE TDB_BLAS)
   else()
     message(STATUS "BLAS not found")
