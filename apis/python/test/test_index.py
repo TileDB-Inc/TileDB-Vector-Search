1e8dfaa365d7bfaa58ce1aa89b4904fcfd4f8b08
Nikos Papailiou <nikolaos.papailiou@tiledb.com>
2023-10-19 11:20:28 +0300

    Fix test

--- apis/python/test/test_index.py
+++ apis/python/test/test_index.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tiledb.vector_search.index as ind
 
 from common import *
 
@@ -10,7 +11,8 @@ from tiledb.vector_search import ivf_flat_index
 def test_flat_index(tmp_path):
     uri = os.path.join(tmp_path, "array")
     index = flat_index.create(uri=uri, dimensions=3, vector_type=np.dtype(np.uint8))
-    _, result = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3)
+    result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3)
+    assert {ind.MAX_UINT64} == set(result_i[0])
 
     update_vectors = np.empty([5], dtype=object)
     update_vectors[0] = np.array([0, 0, 0], dtype=np.dtype(np.uint8))
@@ -21,27 +23,19 @@ def test_flat_index(tmp_path):
     index.update_batch(
         vectors=update_vectors, external_ids=np.array([0, 1, 2, 3, 4]))
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3)
-    assert 2 in result_i
-    assert 3 in result_i
-    assert 1 in result_i
+    assert {1, 2, 3}.issubset(set(result_i[0]))
 
     index = index.consolidate_updates()
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3)
-    assert 2 in result_i
-    assert 3 in result_i
-    assert 1 in result_i
+    assert {1, 2, 3}.issubset(set(result_i[0]))
 
     index.delete_batch(external_ids=np.array([1, 3]))
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3)
-    assert 2 in result_i
-    assert 4 in result_i
-    assert 0 in result_i
+    assert {0, 2, 4}.issubset(set(result_i[0]))
 
     index = index.consolidate_updates()
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3)
-    assert 2 in result_i
-    assert 4 in result_i
-    assert 0 in result_i
+    assert {0, 2, 4}.issubset(set(result_i[0]))
 
     update_vectors = np.empty([2], dtype=object)
     update_vectors[0] = np.array([1, 1, 1], dtype=np.dtype(np.uint8))
@@ -49,34 +43,27 @@ def test_flat_index(tmp_path):
     index.update_batch(
         vectors=update_vectors, external_ids=np.array([1, 3]))
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3)
-    assert 2 in result_i
-    assert 3 in result_i
-    assert 1 in result_i
+    assert {1, 2, 3}.issubset(set(result_i[0]))
 
     index = index.consolidate_updates()
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3)
-    assert 2 in result_i
-    assert 3 in result_i
-    assert 1 in result_i
+    assert {1, 2, 3}.issubset(set(result_i[0]))
 
     index.delete_batch(external_ids=np.array([1, 3]))
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3)
-    assert 2 in result_i
-    assert 4 in result_i
-    assert 0 in result_i
+    assert {0, 2, 4}.issubset(set(result_i[0]))
 
     index = index.consolidate_updates()
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3)
-    assert 2 in result_i
-    assert 4 in result_i
-    assert 0 in result_i
+    assert {0, 2, 4}.issubset(set(result_i[0]))
 
 
 def test_ivf_flat_index(tmp_path):
     partitions = 10
     uri = os.path.join(tmp_path, "array")
     index = ivf_flat_index.create(uri=uri, dimensions=3, vector_type=np.dtype(np.uint8), partitions=partitions)
-    _, result = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3)
+    result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3, nprobe=partitions)
+    assert {ind.MAX_UINT64} == set(result_i[0])
 
     update_vectors = np.empty([5], dtype=object)
     update_vectors[0] = np.array([0, 0, 0], dtype=np.dtype(np.uint8))
@@ -87,27 +74,19 @@ def test_ivf_flat_index(tmp_path):
     index.update_batch(
         vectors=update_vectors, external_ids=np.array([0, 1, 2, 3, 4]))
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3, nprobe=partitions)
-    assert 2 in result_i
-    assert 3 in result_i
-    assert 1 in result_i
+    assert {1, 2, 3}.issubset(set(result_i[0]))
 
     index = index.consolidate_updates()
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3, nprobe=partitions)
-    assert 2 in result_i
-    assert 3 in result_i
-    assert 1 in result_i
+    assert {1, 2, 3}.issubset(set(result_i[0]))
 
     index.delete_batch(external_ids=np.array([1, 3]))
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3, nprobe=partitions)
-    assert 2 in result_i
-    assert 4 in result_i
-    assert 0 in result_i
+    assert {0, 2, 4}.issubset(set(result_i[0]))
 
     index = index.consolidate_updates()
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3, nprobe=partitions)
-    assert 2 in result_i
-    assert 4 in result_i
-    assert 0 in result_i
+    assert {0, 2, 4}.issubset(set(result_i[0]))
 
     update_vectors = np.empty([2], dtype=object)
     update_vectors[0] = np.array([1, 1, 1], dtype=np.dtype(np.uint8))
@@ -115,24 +94,16 @@ def test_ivf_flat_index(tmp_path):
     index.update_batch(
         vectors=update_vectors, external_ids=np.array([1, 3]))
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3, nprobe=partitions)
-    assert 2 in result_i
-    assert 3 in result_i
-    assert 1 in result_i
+    assert {1, 2, 3}.issubset(set(result_i[0]))
 
     index = index.consolidate_updates()
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3, nprobe=partitions)
-    assert 2 in result_i
-    assert 3 in result_i
-    assert 1 in result_i
+    assert {1, 2, 3}.issubset(set(result_i[0]))
 
     index.delete_batch(external_ids=np.array([1, 3]))
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3, nprobe=partitions)
-    assert 2 in result_i
-    assert 4 in result_i
-    assert 0 in result_i
+    assert {0, 2, 4}.issubset(set(result_i[0]))
 
     index = index.consolidate_updates()
     result_d, result_i = index.query(np.array([[2, 2, 2]], dtype=np.float32), k=3, nprobe=partitions)
-    assert 2 in result_i
-    assert 4 in result_i
-    assert 0 in result_i
+    assert {0, 2, 4}.issubset(set(result_i[0]))
