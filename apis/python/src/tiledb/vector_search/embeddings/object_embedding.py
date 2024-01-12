2b4b8ecd2dd2c34636a7a9813e0c113ea55bc5cb
Nikos Papailiou <nikolaos.papailiou@tiledb.com>
2024-01-12 16:05:48 +0200

    Use abstract classes

--- apis/python/src/tiledb/vector_search/embeddings/object_embedding.py
+++ apis/python/src/tiledb/vector_search/embeddings/object_embedding.py
@@ -1,15 +1,21 @@
 import numpy as np
+from abc import ABC, abstractmethod 
 
 
-class ObjectEmbedding:
+class ObjectEmbedding(ABC):
+
+    @abstractmethod
     def dimensions(self) -> int:
         raise NotImplementedError
 
+    @abstractmethod
     def vector_type(self) -> np.dtype:
         raise NotImplementedError
 
+    @abstractmethod
     def load(self):
         raise NotImplementedError
 
+    @abstractmethod
     def embed(self, objects, metadata=None) -> np.ndarray:
         raise NotImplementedError
--- apis/python/src/tiledb/vector_search/object_readers/object_reader.py
+++ apis/python/src/tiledb/vector_search/object_readers/object_reader.py
@@ -1,18 +1,23 @@
+from abc import ABC, abstractmethod 
 from typing import Any, Mapping, Optional, List
 
 
-class ObjectPartition:
+class ObjectPartition(ABC):
+
+    @abstractmethod
     def size(self):
         raise NotImplementedError
 
+    @abstractmethod
     def id(self):
         raise NotImplemented
 
+    @abstractmethod
     def index_slice(self):
         raise NotImplemented
 
 
-class ObjectReader:
+class ObjectReader(ABC):
     def __init__(
         self,
         uri: str,
@@ -23,23 +28,30 @@ class ObjectReader:
         self.config = config
         self.timestamp = timestamp
 
+    @abstractmethod
     def size(self):
         raise NotImplementedError
 
+    @abstractmethod
     def metadata_schema(self):
         raise NotImplementedError
 
+    @abstractmethod
     def metadata_array_uri(self):
         raise NotImplementedError
     
+    @abstractmethod
     def metadata_array_object_id_dim(self):
         raise NotImplementedError
 
+    @abstractmethod
     def get_partitions(self, partition_size: int = -1) -> List[ObjectPartition]:
         raise NotImplementedError
 
+    @abstractmethod
     def read_objects(self, partition: ObjectPartition):
         raise NotImplementedError
 
+    @abstractmethod
     def read_objects_by_ids(self, ids):
         raise NotImplementedError
