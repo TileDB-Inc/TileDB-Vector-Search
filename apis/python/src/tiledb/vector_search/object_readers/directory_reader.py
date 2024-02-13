1d0c9c65976feccb9a1041de22014df6e61e1b0a
Nikos Papailiou <nikolaos.papailiou@tiledb.com>
2024-02-13 16:46:23 +0200

    Format

--- apis/python/src/tiledb/vector_search/object_readers/directory_reader.py
+++ apis/python/src/tiledb/vector_search/object_readers/directory_reader.py
@@ -1,10 +1,12 @@
-from typing import Any, Dict, List, Mapping, Optional, OrderedDict, Tuple, Union, Sequence
 from pathlib import Path
+from typing import Any, Dict, List, Mapping, Optional, OrderedDict, Sequence, Tuple
 
 import numpy as np
+
 import tiledb
 
 # from tiledb.vector_search.object_readers import ObjectPartition, ObjectReader
+
 
 # class DirectoryPartition(ObjectPartition):
 class DirectoryPartition:
@@ -65,7 +67,7 @@ class DirectoryReader:
     def metadata_array_uri(self) -> str:
         return None
 
-    def list_paths(self, vfs : tiledb.VFS, uri):
+    def list_paths(self, vfs: tiledb.VFS, uri):
         children = vfs.ls(uri)
         uri_path = Path(uri)
         for child in children:
@@ -78,13 +80,13 @@ class DirectoryReader:
             if vfs.is_file(child):
                 if not child_path.match(self.glob):
                     continue
-                
+
                 if self.suffixes and child_path.suffix not in self.suffixes:
                     continue
                 self.paths.append(child)
             else:
                 self.list_paths(vfs, child)
-    
+
     def get_partitions(
         self, files_per_partition: int = -1, **kwargs
     ) -> List[DirectoryPartition]:
@@ -115,6 +117,7 @@ class DirectoryReader:
 
 class DirectoryTextReader(DirectoryReader):
     MAX_OBJECTS_PER_FILE = 10000
+
     def __init__(
         self,
         uri: str,
@@ -123,7 +126,10 @@ class DirectoryTextReader(DirectoryReader):
         suffixes: Optional[Sequence[str]] = None,
         config: Optional[Mapping[str, Any]] = None,
         text_splitter: str = "RecursiveCharacterTextSplitter",
-        text_splitter_kwargs: Optional[Dict] = {"chunk_size":1000, "chunk_overlap":100},
+        text_splitter_kwargs: Optional[Dict] = {
+            "chunk_size": 1000,
+            "chunk_overlap": 100,
+        },
     ):
         super().__init__(
             uri=uri,
@@ -155,10 +161,10 @@ class DirectoryTextReader(DirectoryReader):
     def read_objects(
         self, partition: DirectoryPartition
     ) -> Tuple[OrderedDict, OrderedDict]:
-        from urllib.parse import urlparse
         import os
         import tempfile
         import traceback
+        from urllib.parse import urlparse
 
         max_size = DirectoryTextReader.MAX_OBJECTS_PER_FILE * len(partition.paths)
         texts = np.empty(max_size, dtype="O")
@@ -174,24 +180,41 @@ class DirectoryTextReader(DirectoryReader):
                     parsed_path = urlparse(path, allow_fragments=False)
                     if parsed_path.scheme != "file":
                         # Copy remote file to a local temp file
-                        key = parsed_path.path.lstrip('/')
+                        key = parsed_path.path.lstrip("/")
                         local_temp_path = f"{temp_dir}/{key}"
                         os.makedirs(os.path.dirname(local_temp_path), exist_ok=True)
-                        with vfs.open(path, 'rb') as src, open(local_temp_path, 'wb') as dst:
+                        with vfs.open(path, "rb") as src, open(
+                            local_temp_path, "wb"
+                        ) as dst:
                             dst.write(src.read())
                         parsed_path = urlparse(local_temp_path, allow_fragments=False)
-                    if parsed_path.path.endswith(".jpg") or parsed_path.path.endswith(".png"):
-                        from langchain_community.document_loaders import UnstructuredFileLoader
-                        loader = UnstructuredFileLoader(
-                            file_path=parsed_path.path
+                    if parsed_path.path.endswith(".jpg") or parsed_path.path.endswith(
+                        ".png"
+                    ):
+                        from langchain_community.document_loaders import (
+                            UnstructuredFileLoader,
                         )
+
+                        loader = UnstructuredFileLoader(file_path=parsed_path.path)
                     else:
-                        from langchain_community.document_loaders.generic import GenericLoader
-                        from langchain_community.document_loaders.parsers.generic import MimeTypeBasedParser
-                        from langchain_community.document_loaders.parsers.msword import MsWordParser
-                        from langchain_community.document_loaders.parsers.pdf import PyMuPDFParser
-                        from langchain_community.document_loaders.parsers.txt import TextParser
-                        from langchain_community.document_loaders.parsers.html import BS4HTMLParser
+                        from langchain_community.document_loaders.generic import (
+                            GenericLoader,
+                        )
+                        from langchain_community.document_loaders.parsers.generic import (
+                            MimeTypeBasedParser,
+                        )
+                        from langchain_community.document_loaders.parsers.html import (
+                            BS4HTMLParser,
+                        )
+                        from langchain_community.document_loaders.parsers.msword import (
+                            MsWordParser,
+                        )
+                        from langchain_community.document_loaders.parsers.pdf import (
+                            PyMuPDFParser,
+                        )
+                        from langchain_community.document_loaders.parsers.txt import (
+                            TextParser,
+                        )
 
                         loader = GenericLoader.from_filesystem(
                             path=parsed_path.path,
@@ -206,38 +229,42 @@ class DirectoryTextReader(DirectoryReader):
                                     ),
                                 },
                                 fallback_parser=None,
-                            )
+                            ),
                         )
                     documents = loader.load()
                     import importlib
-                    text_splitters_module = importlib.import_module("langchain.text_splitter")
+
+                    text_splitters_module = importlib.import_module(
+                        "langchain.text_splitter"
+                    )
                     splitter_class_ = getattr(text_splitters_module, self.text_splitter)
                     splitter = splitter_class_(**self.text_splitter_kwargs)
                     documents = splitter.split_documents(documents)
-                    size = len(documents)
+                    len(documents)
                     for d in documents:
                         file_paths[write_id] = str(path)
                         if "page" in d.metadata:
                             pages[write_id] = int(d.metadata["page"])
-                        texts[write_id] = f"file_name: {os.path.basename(parsed_path.path)}, page: {pages[write_id]}\n text: {d.page_content}"
-                        external_ids[write_id] = partition.object_id_start * DirectoryTextReader.MAX_OBJECTS_PER_FILE + write_id
+                        texts[write_id] = d.page_content
+                        external_ids[write_id] = (
+                            partition.object_id_start
+                            * DirectoryTextReader.MAX_OBJECTS_PER_FILE
+                            + write_id
+                        )
                         write_id += 1
                     if local_temp_path != "":
                         os.remove(local_temp_path)
-                except Exception as e:
+                except Exception:
                     traceback.print_exc()
         return (
-                {
-                    "text": texts[0:write_id], 
-                    "external_id": external_ids[0:write_id]
-                }, 
-                {
-                    "text": texts[0:write_id], 
-                    "file_path": file_paths[0:write_id], 
-                    "page": pages[0:write_id],
-                    "external_id": external_ids[0:write_id]
-                }
-            )
+            {"text": texts[0:write_id], "external_id": external_ids[0:write_id]},
+            {
+                "text": texts[0:write_id],
+                "file_path": file_paths[0:write_id],
+                "page": pages[0:write_id],
+                "external_id": external_ids[0:write_id],
+            },
+        )
 
     def read_objects_by_external_ids(self, ids: List[int]) -> OrderedDict:
         # TODO implement
@@ -279,6 +306,7 @@ class DirectoryImageReader(DirectoryReader):
         self, partition: DirectoryPartition
     ) -> Tuple[OrderedDict, OrderedDict]:
         from PIL import Image
+
         import tiledb
 
         size = len(partition.paths)
@@ -295,22 +323,23 @@ class DirectoryImageReader(DirectoryReader):
                 shapes[write_id] = np.array(image.shape, dtype=np.uint32)
                 external_ids[write_id] = partition.object_id_start + write_id
                 file_paths[write_id] = str(path)
-                write_id+=1 
+                write_id += 1
         return (
-                {
-                    "image": images[0:write_id], 
-                    "shape": shapes[0:write_id], 
-                    "external_id": external_ids[0:write_id]
-                }, 
-                {
-                    "shape": shapes[0:write_id], 
-                    "file_path": file_paths[0:write_id], 
-                    "external_id": external_ids[0:write_id]
-                }
-            )
+            {
+                "image": images[0:write_id],
+                "shape": shapes[0:write_id],
+                "external_id": external_ids[0:write_id],
+            },
+            {
+                "shape": shapes[0:write_id],
+                "file_path": file_paths[0:write_id],
+                "external_id": external_ids[0:write_id],
+            },
+        )
 
     def read_objects_by_external_ids(self, ids: List[int]) -> OrderedDict:
         from PIL import Image
+
         import tiledb
 
         if self.paths is None:
@@ -332,9 +361,9 @@ class DirectoryImageReader(DirectoryReader):
                 shapes[write_id] = np.array(image.shape, dtype=np.uint32)
                 external_ids[write_id] = id
                 file_paths[write_id] = str(path)
-                write_id+=1 
+                write_id += 1
         return {
-                "image": images[0:write_id], 
-                "shape": shapes[0:write_id], 
-                "external_id": external_ids[0:write_id]
-            }
+            "image": images[0:write_id],
+            "shape": shapes[0:write_id],
+            "external_id": external_ids[0:write_id],
+        }
