d1cf7f6449dfdfe389d4496078b93841649546df
Nikos Papailiou <115017354+NikolaosPapailiou@users.noreply.github.com>
2024-02-14 15:01:46 +0200

    Add directory document readers for vector search Object API (#226)
    This is adding directory readers that can be used to read documents from a directory structure. 
    We introduce 2 readers:
    
    1. Text reader: Extracts text from different types of documents based on their mime type. This can be used for several file types (.pdf, .docx, .html, .jpg, .png, etc.). This is implemented by reusing the LangChain document read utilities.
    2. Image document reader: Read images from image files in a directory. 
    
    The readers also work for vfs directories.
    
    We also introduce multiple notebook examples in `apis/python/examples/object_api/`

--- apis/python/src/tiledb/vector_search/object_readers/__init__.py
+++ apis/python/src/tiledb/vector_search/object_readers/__init__.py
@@ -1,5 +1,9 @@
 from .bioimage_reader import BioImagePartition
 from .bioimage_reader import BioImageReader
+from .directory_reader import DirectoryImageReader
+from .directory_reader import DirectoryPartition
+from .directory_reader import DirectoryReader
+from .directory_reader import DirectoryTextReader
 from .object_reader import ObjectPartition
 from .object_reader import ObjectReader
 from .soma_reader import SomaRNAXRowPartition
@@ -16,4 +20,8 @@ __all__ = [
     "TileDB1DArrayReader",
     "BioImagePartition",
     "BioImageReader",
+    "DirectoryReader",
+    "DirectoryTextReader",
+    "DirectoryImageReader",
+    "DirectoryPartition",
 ]
