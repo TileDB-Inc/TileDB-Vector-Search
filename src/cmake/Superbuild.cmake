226b3eacd68636261c316d8437d5afe1f596f453
Paris Morgan <jparismorgan@gmail.com>
2024-04-02 12:02:45 +0200

    Fix unit_slicing and re-enable in CI (#303)

--- src/cmake/Superbuild.cmake
+++ src/cmake/Superbuild.cmake
@@ -156,5 +156,5 @@ add_custom_target(check
 )
 
 add_custom_target(check-ci
-  COMMAND ${CMAKE_CTEST_COMMAND} -C $<CONFIG> --test-dir ${CMAKE_CURRENT_BINARY_DIR}/libtiledbvectorsearch --output-on-failure --extra-verbose -E \"unit_slicing|unit_ivf_index\"
+  COMMAND ${CMAKE_CTEST_COMMAND} -C $<CONFIG> --test-dir ${CMAKE_CURRENT_BINARY_DIR}/libtiledbvectorsearch --output-on-failure --extra-verbose
 )
