6d7399d272829d47a81166a06914edcdbe40bb60
Paris Morgan <jparismorgan@gmail.com>
2024-04-10 13:22:56 +0200

    fix merge conflict

--- src/include/api/ivf_flat_index.h
+++ src/include/api/ivf_flat_index.h
@@ -437,6 +437,10 @@ class IndexIVFFlat {
     index_->write_index(ctx, group_uri, storage_version);
   }
 
+  constexpr auto dimension() const {
+    return dimension_;  //::dimension(*index_);
+  }
+
   constexpr auto num_partitions() const {
     return nlist_;  // ::num_partitions(*index_);
   }
