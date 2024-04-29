d9bc472820d09107caf8ad608d38ab73deb9eaef
Paris Morgan <jparismorgan@gmail.com>
2024-04-29 11:09:56 +0200

    Do not create default  `tiledb::Config` objects in C++ (#350)

--- src/include/index/flatpq_index.h
+++ src/include/index/flatpq_index.h
@@ -343,8 +343,7 @@ class flatpq_index {
    * Load constructor
    */
   flatpq_index(tiledb::Context ctx, const std::string& group_uri) {
-    tiledb::Config cfg;
-    auto read_group = tiledb::Group(ctx, group_uri, TILEDB_READ, cfg);
+    auto read_group = tiledb::Group(ctx, group_uri, TILEDB_READ, ctx.config());
 
     for (auto& [name, value, datatype] : metadata) {
       if (!read_group.has_metadata(name, &datatype)) {
@@ -643,9 +642,9 @@ class flatpq_index {
   auto write_index(const std::string& group_uri) {
     tiledb::Context ctx;
 
-    tiledb::Config cfg;
     tiledb::Group::create(ctx, group_uri);
-    auto write_group = tiledb::Group(ctx, group_uri, TILEDB_WRITE, cfg);
+    auto write_group =
+        tiledb::Group(ctx, group_uri, TILEDB_WRITE, ctx.config());
 
     for (auto&& [name, value, type] : metadata) {
       write_group.put_metadata(name, type, 1, value);
