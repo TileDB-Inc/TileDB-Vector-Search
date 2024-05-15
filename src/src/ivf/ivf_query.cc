42401b77ebedca0a4aed958ec1dc616a01301bcc
Paris Morgan <jparismorgan@gmail.com>
2024-05-15 14:36:35 -0400

    cleanup code

--- src/src/ivf/ivf_query.cc
+++ src/src/ivf/ivf_query.cc
@@ -177,7 +177,6 @@ int main(int argc, char* argv[]) {
         std::cout << std::endl;
 
         debug_matrix(groundtruth, "groundtruth");
-        debug_matrix(groundtruth, "groundtruth");
 
         std::cout << std::endl;
         debug_matrix(top_k, "top_k");
