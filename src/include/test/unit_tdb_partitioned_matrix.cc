026a549fb5680a6e6c569ac7fdb6da28e3b0ffca
Paris Morgan <jparismorgan@gmail.com>
2024-05-16 14:55:29 -0400

    new test

--- src/include/test/unit_tdb_partitioned_matrix.cc
+++ src/include/test/unit_tdb_partitioned_matrix.cc
@@ -41,11 +41,56 @@
 
 using TestTypes = std::tuple<float, double, int, char, size_t, uint32_t>;
 
+/**
+ * Generates all non-empty subsets of the set {0, 1, ..., num_parts-1}.
+ * 
+ * @param num_parts The number of elements in the set {0, 1, ..., num_parts-1}.
+ * @return A vector of vectors containing all non-empty subsets.
+ */
+template <typename T>
+std::vector<std::vector<T>> generateSubsets(int num_parts) {
+    auto max_subsets = 100;
+    std::vector<T> elements(num_parts);
+    std::vector<std::vector<T>> all_subsets;
+
+    // Initialize the elements vector with values from 0 to num_parts-1
+    for (int i = 0; i < num_parts; ++i) {
+        elements[i] = static_cast<T>(i);
+    }
+
+    // Generate all non-empty subsets
+    for (int subset_size = 1; subset_size <= num_parts; ++subset_size) {
+        std::vector<T> subset(subset_size);
+        std::vector<bool> selection(num_parts, false);
+        for (int i = 0; i < subset_size; ++i) {
+            selection[i] = true;
+        }
+
+        do {
+            int index = 0;
+            for (int i = 0; i < num_parts; ++i) {
+                if (selection[i]) {
+                    subset[index++] = elements[i];
+                }
+            }
+            all_subsets.push_back(subset);
+
+            if (all_subsets.size() >= max_subsets) {
+              return all_subsets;
+            }
+
+        } while (std::prev_permutation(selection.begin(), selection.end()));
+    }
+
+    return all_subsets;
+}
+
 TEST_CASE("tdb_partitioned_matrix: test test", "[tdb_partitioned_matrix]") {
   REQUIRE(true);
 }
 
-TEST_CASE("tdb_partitioned_matrix: uri constructor", "[tdb_partitioned_matrix]") {
+TEST_CASE("tdb_partitioned_matrix: can load correctly", "[tdb_partitioned_matrix]") {
+  return;
   tiledb::Context ctx;
   tiledb::VFS vfs(ctx);
 
@@ -133,7 +178,120 @@ TEST_CASE("tdb_partitioned_matrix: uri constructor", "[tdb_partitioned_matrix]")
     CHECK(std::equal(matrix.data(), matrix.data() + matrix.num_vectors() * _cpo::dimension(matrix), std::vector<feature_type>{}.begin()));
     CHECK(std::equal(matrix.ids().begin(), matrix.ids().end(), std::vector<part_index_type>{}.begin()));
     CHECK(std::equal(matrix.indices().begin(), matrix.indices().end(), indices.begin()));
+  }
+}
 
-    debug_partitioned_matrix(matrix);
+TEST_CASE("tdb_partitioned_matrix: generateSubsets", "[tdb_partitioned_matrix]") {
+  SECTION("generateSubsets with num_parts = 0") {
+    auto subsets = generateSubsets<int>(0);
+    REQUIRE(subsets.empty());
+  }
+
+  SECTION("generateSubsets with num_parts = 1") {
+    auto subsets = generateSubsets<int>(1);
+    std::vector<std::vector<int>> expected = {{0}};
+    REQUIRE(subsets == expected);
+  }
+
+  SECTION("generateSubsets with num_parts = 2") {
+    auto subsets = generateSubsets<int>(2);
+    std::vector<std::vector<int>> expected = {{0}, {1}, {0, 1}};
+    REQUIRE(subsets == expected);
+  }
+
+  SECTION("generateSubsets with num_parts = 3") {
+    auto subsets = generateSubsets<int>(3);
+    std::vector<std::vector<int>> expected = {
+        {0}, {1}, {2}, {0, 1}, {0, 2}, {1, 2}, {0, 1, 2}};
+    REQUIRE(subsets == expected);
+  }
+
+  SECTION("generateSubsets with num_parts = 4") {
+    auto subsets = generateSubsets<int>(4);
+    std::vector<std::vector<int>> expected = {
+        {0},
+        {1},
+        {2},
+        {3},
+        {0, 1},
+        {0, 2},
+        {0, 3},
+        {1, 2},
+        {1, 3},
+        {2, 3},
+        {0, 1, 2},
+        {0, 1, 3},
+        {0, 2, 3},
+        {1, 2, 3},
+        {0, 1, 2, 3}};
+    REQUIRE(subsets == expected);
+  }
+}
+
+TEST_CASE("tdb_partitioned_matrix: test different combinations", "[tdb_partitioned_matrix]") {
+  tiledb::Context ctx;
+  tiledb::VFS vfs(ctx);
+
+  using feature_type = uint64_t;
+  using id_type = uint64_t;
+  using part_index_type = uint64_t;
+
+  size_t min_num_vectors = 10;
+  size_t min_dimensions = 5;
+  size_t min_num_parts = 5;
+  size_t max_num_vectors = 20;
+  size_t max_dimensions = 6;
+  size_t max_num_parts = 10;
+
+  std::string partitioned_vectors_uri = (std::filesystem::temp_directory_path() / "partitioned_vectors").string();
+  std::string ids_uri = (std::filesystem::temp_directory_path() / "ids").string();
+
+  for (size_t num_vectors = min_num_vectors; num_vectors < max_num_vectors; ++num_vectors) {
+    for (size_t dimensions = min_dimensions; dimensions < max_dimensions; ++dimensions) {
+      for (size_t num_parts = min_num_parts; num_parts < max_num_parts; ++num_parts) {
+        auto training_set = ColMajorMatrix<feature_type>(dimensions, num_vectors);
+        for (size_t i = 0; i < dimensions; ++i) {
+          for (size_t j = 0; j < num_vectors; ++j) {
+            training_set(i, j) = j;
+          }
+        }
+
+        std::vector<id_type> part_labels(num_vectors, 0);
+        for (size_t i = 0; i < num_vectors; ++i) {
+          part_labels[i] = i % num_parts;
+        }
+
+        auto partitioned_matrix = ColMajorPartitionedMatrix<feature_type, id_type, part_index_type>(training_set, part_labels, num_parts);
+
+        if (vfs.is_dir(partitioned_vectors_uri)) {
+          vfs.remove_dir(partitioned_vectors_uri);
+        }
+        if (vfs.is_dir(ids_uri)) {
+          vfs.remove_dir(ids_uri);
+        }
+        write_matrix(ctx, partitioned_matrix, partitioned_vectors_uri);
+        write_vector(ctx, partitioned_matrix.ids(), ids_uri);
+
+        // We have num_parts partitions. Create combinations of them. i.e. for num_parts = 3: [0], [1], [2], [0, 1], [0, 2], [1, 2], [0, 1, 2].
+        auto relevant_parts_combinations = generateSubsets<part_index_type>(num_parts);
+        for (auto relevant_parts: relevant_parts_combinations) {
+          auto tdb_partitioned_matrix = tdbColMajorPartitionedMatrix<feature_type, id_type, part_index_type>(
+              ctx,
+              partitioned_vectors_uri,
+              partitioned_matrix.indices(),
+              ids_uri,
+              relevant_parts,
+              0);
+          tdb_partitioned_matrix.load();
+
+          auto expected_num_vectors = 0;
+          for (auto part: relevant_parts) {
+            expected_num_vectors += partitioned_matrix.indices()[part + 1] - partitioned_matrix.indices()[part];
+          }
+          CHECK(tdb_partitioned_matrix.num_vectors() == expected_num_vectors);
+          CHECK(tdb_partitioned_matrix.num_partitions() == std::set<part_index_type>(relevant_parts.begin(), relevant_parts.end()).size());
+        }
+      }
+    }
   }
 }
