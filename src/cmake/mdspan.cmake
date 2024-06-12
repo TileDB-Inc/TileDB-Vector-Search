33233171e1d59cac6080e2d5ad7cee21d72a9edd
Nikos Papailiou <115017354+NikolaosPapailiou@users.noreply.github.com>
2024-06-12 11:06:44 +0300

    Pin mdspan version (#413)

--- src/cmake/mdspan.cmake
+++ src/cmake/mdspan.cmake
@@ -9,6 +9,6 @@ include(FetchContent)
 FetchContent_Declare(
         mdspan
         GIT_REPOSITORY https://github.com/kokkos/mdspan.git
-        GIT_TAG stable)
+        GIT_TAG 96b3985b291c2ed3e24f22302d5a372522655a2c)
 
 FetchContent_MakeAvailable(mdspan)
