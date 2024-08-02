bff8941acf5b1d773c649f13399bbbf759e4d56c
Paris Morgan <jparismorgan@gmail.com>
2024-08-02 12:30:46 +0200

    lint

--- src/benchmarks/ann-benchmarks.py
+++ src/benchmarks/ann-benchmarks.py
@@ -47,7 +47,9 @@ if also_benchmark_others:
         "pgvector",
     ]
 
-# If you want to skip running benchmarks and keep the instance running after the script finishes, set this to True. This is useful if you want to SSH into the instance and make changes to ann-benchmarks code. Make sure to terminate the instance manually on AWS when you are done.
+# If you want to skip running benchmarks and keep the instance running after the script finishes,
+# set this to True. This is useful if you want to SSH into the instance and make changes to
+# ann-benchmarks code. Make sure to terminate the instance manually on AWS when you are done.
 skip_benchmarks_and_leave_running = False
 
 # You must set these before running the script:
