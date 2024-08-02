6036f2c69351c229d3598d4aa2bccb3ec7f0e4c8
Paris Morgan <jparismorgan@gmail.com>
2024-08-02 11:36:23 +0200

    enable more tests

--- src/include/test/unit_array_defs.cc
+++ src/include/test/unit_array_defs.cc
@@ -52,108 +52,108 @@ std::vector<std::string> test_file_roots{
     siftsmall_files_root,
 };
 
-// TEST_CASE("test array root uris", "[array_defs]") {
-//   for (auto& uri : {siftsmall_root, bigann10k_root}) {
-//     REQUIRE(std::filesystem::is_directory(uri));
-//   }
-//   for (auto& uri : {siftsmall_files_root}) {
-//     REQUIRE(std::filesystem::is_directory(uri));
-//   }
-// }
+TEST_CASE("test array root uris", "[array_defs]") {
+  for (auto& uri : {siftsmall_root, bigann10k_root}) {
+    REQUIRE(std::filesystem::is_directory(uri));
+  }
+  for (auto& uri : {siftsmall_files_root}) {
+    REQUIRE(std::filesystem::is_directory(uri));
+  }
+}
 
-// std::vector<std::string> sift_array_uris{
-//     sift_inputs_uri,
-//     sift_centroids_uri,
-//     sift_index_uri,
-//     sift_ids_uri,
-//     sift_parts_uri,
-//     sift_query_uri,
-//     sift_groundtruth_uri,
-// };
+std::vector<std::string> sift_array_uris{
+    sift_inputs_uri,
+    sift_centroids_uri,
+    sift_index_uri,
+    sift_ids_uri,
+    sift_parts_uri,
+    sift_query_uri,
+    sift_groundtruth_uri,
+};
 
-// std::vector<std::string> siftsmall_array_uris{
-//     siftsmall_inputs_uri,
-//     siftsmall_centroids_uri,
-//     siftsmall_index_uri,
-//     siftsmall_ids_uri,
-//     siftsmall_parts_uri,
-//     siftsmall_query_uri,
-//     siftsmall_groundtruth_uri,
-// };
+std::vector<std::string> siftsmall_array_uris{
+    siftsmall_inputs_uri,
+    siftsmall_centroids_uri,
+    siftsmall_index_uri,
+    siftsmall_ids_uri,
+    siftsmall_parts_uri,
+    siftsmall_query_uri,
+    siftsmall_groundtruth_uri,
+};
 
-// // Note that we don't have a canonical IVF index for bigann10k yet, so some
-// // of these URIs are placeholders
-// std::vector<std::string> bigann10k_array_uris{
-//     bigann10k_inputs_uri,
-//     bigann10k_centroids_uri,
-//     bigann10k_index_uri,
-//     bigann10k_ids_uri,
-//     bigann10k_parts_uri,
-//     bigann10k_query_uri,
-//     bigann10k_groundtruth_uri,
-// };
+// Note that we don't have a canonical IVF index for bigann10k yet, so some
+// of these URIs are placeholders
+std::vector<std::string> bigann10k_array_uris{
+    bigann10k_inputs_uri,
+    bigann10k_centroids_uri,
+    bigann10k_index_uri,
+    bigann10k_ids_uri,
+    bigann10k_parts_uri,
+    bigann10k_query_uri,
+    bigann10k_groundtruth_uri,
+};
 
-// std::vector<std::string> bigann1M_array_uris{
-//     bigann1M_inputs_uri,
-//     bigann1M_centroids_uri,
-//     bigann1M_index_uri,
-//     bigann1M_ids_uri,
-//     bigann1M_parts_uri,
-//     bigann1M_query_uri,
-//     bigann1M_groundtruth_uri,
-// };
+std::vector<std::string> bigann1M_array_uris{
+    bigann1M_inputs_uri,
+    bigann1M_centroids_uri,
+    bigann1M_index_uri,
+    bigann1M_ids_uri,
+    bigann1M_parts_uri,
+    bigann1M_query_uri,
+    bigann1M_groundtruth_uri,
+};
 
-// // Note that we don't have a canonical IVF index for fmnist yet, so some
-// // of these URIs are placeholders
-// std::vector<std::string> fmnist_array_uris{
-//     fmnist_inputs_uri,
-//     fmnist_centroids_uri,
-//     fmnist_index_uri,
-//     fmnist_ids_uri,
-//     fmnist_parts_uri,
-//     fmnist_query_uri,
-//     fmnist_groundtruth_uri,
-// };
+// Note that we don't have a canonical IVF index for fmnist yet, so some
+// of these URIs are placeholders
+std::vector<std::string> fmnist_array_uris{
+    fmnist_inputs_uri,
+    fmnist_centroids_uri,
+    fmnist_index_uri,
+    fmnist_ids_uri,
+    fmnist_parts_uri,
+    fmnist_query_uri,
+    fmnist_groundtruth_uri,
+};
 
-// std::vector<std::string> siftsmall_files{
-//     siftsmall_inputs_file,
-//     siftsmall_query_file,
-//     siftsmall_groundtruth_file,
-// };
+std::vector<std::string> siftsmall_files{
+    siftsmall_inputs_file,
+    siftsmall_query_file,
+    siftsmall_groundtruth_file,
+};
 
-// TEST_CASE("test array uris", "[array_defs]") {
-//   bool debug = false;
+TEST_CASE("test array uris", "[array_defs]") {
+  bool debug = false;
 
-//   for (auto& test :
-//        {siftsmall_array_uris,
-//         // Not checked in to github
-//         // siftsmall_uint8_array_uris,
-//         bigann10k_array_uris}) {
-//     for (auto& uri : test) {
-//       if (debug) {
-//         std::cout << uri << " "
-//                   << (std::filesystem::exists(uri) ? "exists" :
-//                                                      "does not exist");
-//         std::cout << " and is directory is "
-//                   << std::filesystem::is_directory(uri) << " ";
-//         std::cout << std::endl;
-//       }
-//       // CHECK(std::filesystem::exists(uri));
+  for (auto& test :
+       {siftsmall_array_uris,
+        // Not checked in to github
+        // siftsmall_uint8_array_uris,
+        bigann10k_array_uris}) {
+    for (auto& uri : test) {
+      if (debug) {
+        std::cout << uri << " "
+                  << (std::filesystem::exists(uri) ? "exists" :
+                                                     "does not exist");
+        std::cout << " and is directory is "
+                  << std::filesystem::is_directory(uri) << " ";
+        std::cout << std::endl;
+      }
+      // CHECK(std::filesystem::exists(uri));
 
-//       CHECK(std::filesystem::is_directory(uri));
-//     }
-//   }
+      CHECK(std::filesystem::is_directory(uri));
+    }
+  }
 
-//   for (auto& file : siftsmall_files) {
-//     if (debug) {
-//       std::cout << file << " "
-//                 << (std::filesystem::exists(file) ? "exists" : "does not
-//                 exist")
-//                 << std::endl;
-//     }
-//     CHECK(std::filesystem::is_regular_file(file));
-//   }
-// }
+  for (auto& file : siftsmall_files) {
+    if (debug) {
+      std::cout << file << " "
+                << (std::filesystem::exists(file) ? "exists" : "does not
+                exist")
+                << std::endl;
+    }
+    CHECK(std::filesystem::is_regular_file(file));
+  }
+}
 
 TEST_CASE("compare siftsmall arrays and files", "[array_defs]") {
   tiledb::Context ctx;
