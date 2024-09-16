95b91361a6614e9bec67c5a918ecd277736dfae7
Paris Morgan <jparismorgan@gmail.com>
2024-09-16 13:42:55 +0200

    fix avx2 and improve tests

--- src/include/test/unit_inner_product_distance.cc
+++ src/include/test/unit_inner_product_distance.cc
@@ -88,67 +88,7 @@ TEST_CASE("simple vectors", "[inner_product_distance]") {
 #endif
 }
 
-TEST_CASE("negative values", "[inner_product_distance]") {
-  auto x = std::vector<float>{
-      -126.f, -121.f, -101.f, 127.f,  45.f,   -73.f,  -119.f, -128.f, -51.f,
-      -88.f,  -86.f,  -90.f,  -114.f, -75.f,  -11.f,  -108.f, -111.f, -125.f,
-      -128.f, -128.f, -125.f, -52.f,  76.f,   -89.f,  -76.f,  -123.f, -120.f,
-      -123.f, -119.f, -111.f, -82.f,  -57.f,  -126.f, -125.f, -112.f, -82.f,
-      120.f,  78.f,   -15.f,  -123.f, -128.f, -128.f, -108.f, 26.f,   -43.f,
-      62.f,   78.f,   -122.f, -78.f,  -125.f, -126.f, -107.f, -100.f, -94.f,
-      106.f,  83.f,   4.f,    -96.f,  -119.f, -120.f, -123.f, -124.f, -110.f,
-      90.f,   -98.f,  -122.f, -112.f, -87.f,  -112.f, -97.f,  5.f,    9.f,
-      -101.f, -128.f, -106.f, 92.f,   37.f,   -128.f, -124.f, -87.f,  25.f,
-      -60.f,  -85.f,  55.f,   -39.f,  -126.f, -121.f, -93.f,  78.f,   -19.f,
-      -117.f, -106.f, -60.f,  -102.f, -114.f, -78.f,  -43.f,  -86.f,  -103.f,
-      -107.f, -128.f, -128.f, -91.f,  -40.f,  24.f,   -79.f,  -99.f,  93.f,
-      -83.f,  -116.f, -124.f, -77.f,  -106.f, 0.f,    5.f,    60.f,   -73.f,
-      -106.f, -124.f, -124.f, -78.f,  -84.f,  -111.f, -118.f, -59.f,  -48.f,
-      -18.f,  -105.f};
-  auto y = std::vector<float>{
-      -59.f,  -74.f,  -101.f, -128.f, -66.f,  -51.f,  -115.f, -112.f, -110.f,
-      -122.f, -128.f, -125.f, -59.f,  43.f,   32.f,   -88.f,  -70.f,  -25.f,
-      -99.f,  -126.f, -115.f, -80.f,  13.f,   -61.f,  -125.f, -76.f,  -108.f,
-      -112.f, -86.f,  -52.f,  26.f,   -125.f, -24.f,  -105.f, -109.f, -120.f,
-      127.f,  -47.f,  -90.f,  -28.f,  -112.f, -126.f, -126.f, -98.f,  107.f,
-      81.f,   -3.f,   -76.f,  15.f,   -98.f,  -122.f, -124.f, -122.f, -54.f,
-      96.f,   74.f,   -89.f,  -120.f, -108.f, -106.f, -125.f, -67.f,  6.f,
-      -80.f,  -45.f,  -112.f, -114.f, -72.f,  -12.f,  -75.f,  -100.f, -47.f,
-      -120.f, -115.f, -29.f,  81.f,   102.f,  -68.f,  -126.f, -128.f, 32.f,
-      15.f,   -1.f,   -56.f,  -119.f, -124.f, -124.f, -72.f,  -69.f,  -34.f,
-      -73.f,  -84.f,  -123.f, -128.f, -121.f, -89.f,  -23.f,  -2.f,   -78.f,
-      -79.f,  -104.f, -75.f,  -118.f, -113.f, -108.f, -81.f,  51.f,   10.f,
-      -27.f,  53.f,   -55.f,  -100.f, -86.f,  41.f,   61.f,   -107.f, -121.f,
-      -102.f, -17.f,  -48.f,  -120.f, -55.f,  -79.f,  23.f,   -92.f,  -122.f,
-      -84.f,  -100.f};
-  CHECK(x.size() == y.size());
-  debug_vector(x, "x");
-  debug_vector(y, "y");
-  auto n = x.size();
-  float manual = 0.f;
-  for (size_t i = 0; i < n; ++i) {
-    manual += x[i] * y[i];
-  }
-  std::cout << "manual: " << manual << std::endl;
-
-  auto naive_xy = naive_inner_product(x, y);
-  CHECK(std::abs(naive_xy - manual) < 0.01);
-  auto unroll4_xy = unroll4_inner_product(x, y);
-  CHECK(std::abs(unroll4_xy - manual) < 0.01);
-
-  std::cout << "naive: " << naive_xy << std::endl;
-  std::cout << "unroll4: " << unroll4_xy << std::endl;
-
-#ifdef __AVX2__
-  auto avx2_xy = avx2_inner_product(x, y);
-  CHECK(std::abs(avx2_xy - manual) < 0.01);
-  std::cout << "avx2: " << avx2_xy << std::endl;
-#endif
-}
-
 TEST_CASE("simple longer vectors", "[inner_product_distance]") {
-  std::cout << std::fixed << std::setprecision(7);
-
   size_t n = 100;
   auto x = std::vector<float>(n);
   for (size_t i = 0; i < n; ++i) {
@@ -158,26 +98,19 @@ TEST_CASE("simple longer vectors", "[inner_product_distance]") {
   for (size_t i = 0; i < n; ++i) {
     y[i] = i * 10.f;
   }
-  debug_vector(x, "x");
-  debug_vector(y, "y");
-
   float manual = 0.f;
   for (size_t i = 0; i < n; ++i) {
     manual += x[i] * y[i];
   }
-  std::cout << "manual:  " << manual << std::endl;
 
   auto naive_xy = naive_inner_product(x, y);
   auto unroll4_xy = unroll4_inner_product(x, y);
 
-  std::cout << "naive:   " << naive_xy << std::endl;
   CHECK(std::abs(naive_xy - manual) < 0.01);
-  std::cout << "unroll4: " << unroll4_xy << std::endl;
   CHECK(std::abs(unroll4_xy - manual) < 0.01);
 
 #ifdef __AVX2__
   auto avx2_xy = avx2_inner_product(x, y);
-  std::cout << "avx2: " << avx2_xy << std::endl;
 #endif
 }
 
@@ -186,7 +119,7 @@ TEMPLATE_TEST_CASE(
   // size_t n = GENERATE(1, 3, 127, 1021, 1024);
 
   // @todo generate a range of sizes (will require range of expected answers)
-  size_t n = GENERATE(127);
+  size_t n = GENERATE(55);
 
   auto u = std::vector<TestType>(n);
   auto v = std::vector<TestType>(n);
@@ -202,69 +135,76 @@ TEMPLATE_TEST_CASE(
 
   std::iota(begin(z), end(z), 13);
 
-  auto a = naive_inner_product(x, y);
-  CHECK(std::abs(a - 785444.4375) < 0.01);
-  auto ax = naive_inner_product(y, x);
-  CHECK(ax == a);
-  CHECK(std::abs(ax - 785444.4375) < 0.01);
+  {
+    float expected = 72735.734375f;
+    auto naive_xy = naive_inner_product(x, y);
+    CHECK(std::abs(naive_xy - expected) < 0.01);
+    CHECK(naive_xy == naive_inner_product(y, x));
 
-  auto b = unroll4_inner_product(x, y);
-  CHECK(std::abs(b - 785444.4375) < 0.01);
-  CHECK(a == b);
-  auto bx = unroll4_inner_product(y, x);
-  CHECK(bx == b);
-  CHECK(std::abs(bx - 785444.4375) < 0.01);
-
-  auto a2 = naive_inner_product(u, v);
-  CHECK(a2 == 778764);
-  auto b2 = unroll4_inner_product(u, v);
-  CHECK(b2 == 778764);
-  CHECK(a2 == b2);
-
-  auto a2x = naive_inner_product(v, u);
-  CHECK(a2x == 778764);
-  CHECK(a2 == a2x);
-  auto b2x = unroll4_inner_product(v, u);
-  CHECK(b2x == 778764);
-  CHECK(a2x == b2x);
-
-  auto a3 = naive_inner_product(u, x);
-  CHECK(std::abs(a3 - 649615.1875) < 0.25);
-  auto b3 = unroll4_inner_product(u, x);
-  CHECK(std::abs(a3 - 649615.1875) < 0.25);
-  CHECK(std::abs(a3 - b3) < 0.25);
-
-  auto a3x = naive_inner_product(x, u);
-  CHECK(std::abs(a3x - 649615.1875) < 0.25);
-  CHECK(a3 == a3x);
-  auto b3x = unroll4_inner_product(x, u);
-  CHECK(std::abs(a3x - 649615.1875) < 0.25);
-  CHECK(std::abs(a3x - b3x) < 0.25);
+    auto unroll4_xy = unroll4_inner_product(x, y);
+    CHECK(std::abs(unroll4_xy - expected) < 0.01);
+    CHECK(unroll4_xy == unroll4_inner_product(y, x));
 
 #ifdef __AVX2__
-  {
-    auto a = avx2_inner_product(x, y);
-    CHECK(std::abs(a - 785444.4375) < 0.07);
-
-    auto ax = avx2_inner_product(y, x);
-    CHECK(ax == a);
-    CHECK(std::abs(ax - 785444.4375) < 0.07);
-
-    auto a2 = avx2_inner_product(u, v);
-    CHECK(a2 == 778764);
-
-    auto a2x = avx2_inner_product(v, u);
-    CHECK(a2x == 778764);
-    CHECK(a2 == a2x);
-
-    auto a3 = avx2_inner_product(u, x);
-    CHECK(std::abs(a3 - 649615.1875) < 0.25);
-
-    auto a3x = avx2_inner_product(x, u);
-    CHECK(std::abs(a3x - 649615.1875) < 0.25);
-    CHECK(a3 == a3x);
-  }
+    auto avx2_xy = avx2_inner_product(x, y);
+    CHECK(std::abs(avx2_xy - expected) < 0.07);
+    CHECK(avx2_xy == avx2_inner_product(y, x));
 #endif
+  }
+
+  {
+    auto naive_uv = naive_inner_product(u, v);
+    CHECK(naive_uv == 73260);
+    CHECK(naive_uv == naive_inner_product(v, u));
+
+    CHECK(naive_uv == unroll4_inner_product(u, v));
+    CHECK(naive_uv == unroll4_inner_product(v, u));
+
+#ifdef __AVX2__
+    CHECK(naive_uv == avx2_inner_product(u, v));
+    CHECK(naive_uv == avx2_inner_product(v, u));
+#endif
+  }
+
+  {
+    float expected = 49289.7382812f;
+    auto naive_ux = naive_inner_product(u, x);
+    CHECK(std::abs(naive_ux - expected) < 0.05);
+    CHECK(naive_ux == naive_inner_product(x, u));
+
+    auto unroll4_ux = unroll4_inner_product(u, x);
+    CHECK(std::abs(unroll4_ux - expected) < 0.05);
+
+    auto unroll4_xu = unroll4_inner_product(x, u);
+    CHECK(std::abs(unroll4_xu - expected) < 0.05);
+
+#ifdef __AVX2__
+    auto avx2_ux = avx2_inner_product(u, x);
+    CHECK(std::abs(avx2_ux - expected) < 0.05);
+
+    CHECK(avx2_ux == avx2_inner_product(x, u));
+#endif
+  }
+
+  {
+    float expected = 112568.59375f;
+    auto naive_vy = naive_inner_product(v, y);
+    CHECK(std::abs(naive_vy - expected) < 0.05);
+    CHECK(naive_vy == naive_inner_product(y, v));
+
+    auto unroll4_vy = unroll4_inner_product(v, y);
+    CHECK(std::abs(unroll4_vy - expected) < 0.05);
+
+    auto unroll4_yv = unroll4_inner_product(y, v);
+    CHECK(std::abs(unroll4_yv - expected) < 0.05);
+
+#ifdef __AVX2__
+    auto avx2_ux = avx2_inner_product(u, x);
+    CHECK(std::abs(avx2_ux - expected) < 0.05);
+
+    CHECK(avx2_ux == avx2_inner_product(x, u));
+#endif
+  }
 
 // @todo: inner_product does not yet have a partitioned version implemented yet.
 // Leaving code here for future reference
--- src/include/test/unit_l2_distance.cc
+++ src/include/test/unit_l2_distance.cc
@@ -31,9 +31,100 @@
  */
 
 #include <catch2/catch_all.hpp>
+#include <iomanip>
 #include "detail/scoring/l2_distance.h"
 
-TEMPLATE_TEST_CASE("naive_sum_of_squares", "[l2_distance]", int8_t, uint8_t) {
+TEMPLATE_TEST_CASE("simple sum_of_squares", "[l2_distance]", int8_t, uint8_t) {
+  std::cout << std::fixed << std::setprecision(7);
+
+  size_t n = GENERATE(55);
+
+  auto u = std::vector<TestType>(n);
+  auto v = std::vector<TestType>(n);
+  auto x = std::vector<float>(n);
+  auto y = std::vector<float>(n);
+
+  std::iota(begin(u), end(u), 0);
+  std::iota(begin(v), end(v), 5);
+  std::iota(begin(x), end(x), -3.14159);
+  std::iota(begin(y), end(y), 2.71828);
+
+  {
+    float expected = 1888.5957031;
+
+    auto naive_xy = naive_sum_of_squares(x, y);
+    CHECK(std::abs(naive_xy - expected) < 0.01);
+    CHECK(naive_xy == naive_sum_of_squares(y, x));
+
+    auto unroll4_xy = unroll4_sum_of_squares(x, y);
+    CHECK(std::abs(unroll4_xy - expected) < 0.01);
+    CHECK(unroll4_xy == unroll4_sum_of_squares(y, x));
+
+#ifdef __AVX2__
+    auto avx2_xy = avx2_sum_of_squares(x, y);
+    CHECK(std::abs(avx2_xy - expected) < 0.01);
+    CHECK(avx2_xy == avx2_sum_of_squares(y, x));
+#endif
+  }
+
+  {
+    float expected = 1375.f;
+
+    auto naive_uv = naive_sum_of_squares(u, v);
+    CHECK(std::abs(naive_uv - expected) < 0.01);
+    CHECK(naive_uv == naive_sum_of_squares(v, u));
+
+    auto unroll4_uv = unroll4_sum_of_squares(u, v);
+    CHECK(std::abs(unroll4_uv - expected) < 0.01);
+    CHECK(unroll4_uv == unroll4_sum_of_squares(v, u));
+
+#ifdef __AVX2__
+    auto avx2_uv = avx2_sum_of_squares(u, v);
+    CHECK(std::abs(avx2_uv - expected) < 0.01);
+    CHECK(avx2_uv == avx2_sum_of_squares(v, u));
+#endif
+  }
+
+  {
+    // Mixed vector types: u (integers) and x (floats)
+    float expected_ux = 542.8275146f;
+
+    auto naive_ux = naive_sum_of_squares(u, x);
+    CHECK(std::abs(naive_ux - expected_ux) < 0.01);
+    CHECK(naive_ux == naive_sum_of_squares(x, u));
+
+    auto unroll4_ux = unroll4_sum_of_squares(u, x);
+    CHECK(std::abs(unroll4_ux - expected_ux) < 0.01);
+    CHECK(unroll4_ux == unroll4_sum_of_squares(x, u));
+
+#ifdef __AVX2__
+    auto avx2_ux = avx2_sum_of_squares(u, x);
+    CHECK(std::abs(avx2_ux - expected_ux) < 0.01);
+    CHECK(avx2_ux == avx2_sum_of_squares(x, u));
+#endif
+  }
+
+  {
+    float expected_vy = 286.3432922f;
+
+    auto naive_vy = naive_sum_of_squares(v, y);
+    CHECK(std::abs(naive_vy - expected_vy) < 0.01);
+    CHECK(naive_vy == naive_sum_of_squares(y, v));
+
+    auto unroll4_vy = unroll4_sum_of_squares(v, y);
+    CHECK(std::abs(unroll4_vy - expected_vy) < 0.01);
+    CHECK(unroll4_vy == unroll4_sum_of_squares(y, v));
+
+#ifdef __AVX2__
+    auto avx2_vy = avx2_sum_of_squares(v, y);
+    CHECK(std::abs(avx2_vy - expected_vy) < 0.01);
+    CHECK(avx2_vy == avx2_sum_of_squares(y, v));
+#endif
+  }
+}
+
+TEMPLATE_TEST_CASE(
+    "start and stop sum_of_squares", "[l2_distance]", int8_t, uint8_t) {
   // size_t n = GENERATE(1, 3, 127, 1021, 1024);
 
   size_t n = GENERATE(127);
